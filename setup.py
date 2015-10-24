c17000360ac5e0f93b16f87b02c2e6ddbc0b968c
Rafael Santos <rafaelpsantos@gmail.com>
2015-10-24 19:48:09 -0200

    version 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PySCD',
-    version='1.0.1',
+    version='1.0.2',
     description=('Slowly Changing Dimension management '
                  'supporting SCD types 1 and 2'),
     url='https://github.com/rtogo/pyscd',
